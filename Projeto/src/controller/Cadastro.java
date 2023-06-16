aa7ec33351277b19f58ff3002219409cd8d19803
lcaioporto <caio.dporto@gmail.com>
2023-06-16 11:18:31 -0300

    Guardar info cadastro em CSV

--- Projeto/src/controller/Cadastro.java
+++ Projeto/src/controller/Cadastro.java
@@ -1,23 +1,36 @@
 package controller;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import model.Usuario;
 
 public class Cadastro {
     private Usuario user;
-    private PrintWriter out;
 
     public Cadastro (String usuario, String nome, String faculdade, String curso, String senha) {
         user = new Usuario(usuario, nome, faculdade,curso, senha);
-        File usersCSV = new File("controller/File/Users.csv");
+        File usersCSV = new File("Projeto\\src\\controller\\Files\\Users.csv");
+        String header = "USER, NOME, INSTITUICAO, CURSO, SENHA\n"; 
+        String conteudo = user.getUsuario() + ", " + user.getNome() + ", " + user.getFaculdade() + ", " + user.getCurso() + ", " + user.getSenha() + "\n";
         try {
-            out = new PrintWriter(usersCSV);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            if (!usersCSV.exists()) {
+                usersCSV.createNewFile();
+                FileWriter fw = new FileWriter(usersCSV, true);
+                try (BufferedWriter bw = new BufferedWriter(fw)) {
+                    bw.write(header);
+                    bw.write(conteudo);
+                }
+            }
+            else {
+                FileWriter fw = new FileWriter(usersCSV, true);
+                try (BufferedWriter bw = new BufferedWriter(fw)) {
+                    bw.write(conteudo);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e);
         }
-        
-        out.printf("%s, %s, %s, %s, %s", user.getUsuario(), user.getNome(), user.getFaculdade(), user.getCurso(), user.getSenha());
-        out.close();
     }
 }
