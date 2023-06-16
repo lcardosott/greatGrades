d567d7b3823ba0fce404e2e85e0b79dd0bb4f2c3
lcaioporto <caio.dporto@gmail.com>
2023-06-16 14:59:53 -0300

    Adicionando getUser em Cadastro

--- Projeto/src/controller/Cadastro.java
+++ Projeto/src/controller/Cadastro.java
@@ -50,4 +50,8 @@ public class Cadastro {
     public static ArrayList<Usuario> getListaUsers() {
         return listaUsers;
     }
+
+    public Usuario getUser () {
+        return user;
+    }
 }
