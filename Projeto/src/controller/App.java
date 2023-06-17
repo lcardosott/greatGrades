68d7cd47e85ace169f8038d8d2bbba6662bd5116
lcaioporto <caio.dporto@gmail.com>
2023-06-17 18:24:06 -0300

    Front-end CadastroMateria pronto

--- Projeto/src/controller/App.java
+++ Projeto/src/controller/App.java
@@ -2,12 +2,12 @@ package controller;
 //import view.firstPage.FrameInicial;
 
 import model.Materia;
-import view.cadastroAvaliacao.CadastroAvaliacao;
+import view.cadastroAvaliacao.FrameCadastroAvaliacao;
 
 public class App {
     public static void main(String[] args) {
         //new FrameInicial();
         Materia materia = new Materia("materiatop", "a", "nomeProf", 5, 6, 0, 1);
-        new CadastroAvaliacao(materia);
+        new FrameCadastroAvaliacao(materia);
     }
 }
