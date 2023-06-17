e4c2b6451cd7fdda51daa73be9045fe60dc9b97c
lcaioporto <caio.dporto@gmail.com>
2023-06-17 20:05:49 -0300

    Adicionando frame para apresentar informações do usuário

--- Projeto/src/controller/App.java
+++ Projeto/src/controller/App.java
@@ -1,13 +1,16 @@
 package controller;
 //import view.firstPage.FrameInicial;
-
-import model.Materia;
-import view.cadastroAvaliacao.FrameCadastroAvaliacao;
+//import model.Materia;
+//import view.cadastroAvaliacao.FrameCadastroAvaliacao;
+import model.Usuario;
+import view.verUsuario.FrameUsuario;
 
 public class App {
     public static void main(String[] args) {
         //new FrameInicial();
-        Materia materia = new Materia("materiatop", "a", "nomeProf", 5, 6, 0, 1);
-        new FrameCadastroAvaliacao(materia);
+        //Materia materia = new Materia("materiatop", "a", "nomeProf", 5, 6, 0, 1);
+        //new FrameCadastroAvaliacao(materia);
+        Usuario user = new Usuario("user", "Caio", "Unicamp", "Eng. Comp.", "senha");
+        new FrameUsuario(user);
     }
 }
